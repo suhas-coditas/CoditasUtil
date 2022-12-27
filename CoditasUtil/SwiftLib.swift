d13adec21332f6ba0a07f815deceaa3e6586e075
Suhas Patil <suhas.patil@coditas.com>
2022-12-27 17:58:20 +0530

    Added test cases

---
+++ CoditasUtil/SwiftLib.swift
@@ -0,0 +1,22 @@
+//
+//  SwiftLib.swift
+//  CoditasUtil
+//
+//  Created by Coditas on 12/27/22.
+//
+
+import Foundation
+
+public final class SwiftyLib {
+
+    let name = "SwiftyLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
