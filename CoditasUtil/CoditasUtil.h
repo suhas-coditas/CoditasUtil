b8f0da67c3243ae4cf3fd8047b38e98717698adf
Suhas Patil <suhas.patil@coditas.com>
2022-12-27 14:30:49 +0530

    Initial Commit

---
+++ CoditasUtil/CoditasUtil.h
@@ -0,0 +1,18 @@
+//
+//  CoditasUtil.h
+//  CoditasUtil
+//
+//  Created by Coditas on 12/27/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CoditasUtil.
+FOUNDATION_EXPORT double CoditasUtilVersionNumber;
+
+//! Project version string for CoditasUtil.
+FOUNDATION_EXPORT const unsigned char CoditasUtilVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoditasUtil/PublicHeader.h>
+
+
