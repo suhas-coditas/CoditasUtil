cdc10c1a74c96ca8d8e3551282c6f24c3bf5cb18
Suhas Patil <suhas.patil@coditas.com>
2022-12-27 14:41:55 +0530

    Initial project setup

---
+++ CoditasYtilExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoditasYtilExample
+//
+//  Created by Coditas on 12/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
